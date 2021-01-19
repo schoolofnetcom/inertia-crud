be11ed958a5a883a165700ed5981a77082f311cb
thivalls <olavalls@gmail.com>
2021-01-19 10:49:14 -0300

    Aula 2 - Entendendo ambiente inicial

--- resources/js/Pages/Book/Create/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Book = () => {
-    return (
-        <h1>Create</h1>
-    )
-}
-
-export default Book;
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,9 @@
 <?php
 
-use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
 Route::group(['middleware' => 'auth'], function(){
     Route::get('/dashboard', function () { return view('dashboard'); })->name('dashboard');
-    Route::resource('books', BookController::class);
 });
 
 require __DIR__.'/auth.php';
